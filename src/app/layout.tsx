3a3e70a4f3b891f4eccaebdad6f9291f3581ac9e
Daniel Johnson <dojohnson@ucdavis.edu>
2025-06-13 23:02:21 -0700

    more

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 import { Topbar } from "@/components/layout/topbar";
 import "./globals.css";
 
