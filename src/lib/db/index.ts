89797ce722ed9188d23a5eba6ef129b3fe941630
Daniel Johnson <dojohnson@ucdavis.edu>
2025-06-13 20:55:32 -0700

    more

---
+++ src/lib/db/index.ts
@@ -0,0 +1,14 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+
+// Create the connection string from Supabase credentials
+const connectionString = process.env.DATABASE_URL!;
+
+// Create postgres client
+const client = postgres(connectionString);
+
+// Create drizzle instance
+export const db = drizzle(client, { schema });
+
+export * from "./schema";
